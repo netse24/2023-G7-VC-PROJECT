ede52a7f46af1779a670a6261c818ff1007620e3
SOKI.CHHUOY <SOKI.CHHUOY@PNC.LAN>
2023-07-04 13:06:37 +0700

    [SMSG-41] create view login from

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,4 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+// import LoginView from './views/login/LoginView.vue'
+
+const app = createApp(App);
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+app.use(vuetify)
+app.use(router)
+app.mount('#app')
