a76d805b34bc6faeb45aeb88dd4597107e834306
soknoeurn-srun <soknoeurn.srun@student.passerellesnumeriques.org>
2023-07-05 10:22:02 +0700

    SMSG-167 <create view of admin page>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <v-app>
+    <!-- sidebar -->
+    <SideBar/>
+    <NavigationBar/>
+    <v-main>
+      <!-- cards -->
+      <AdminPage/>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script setup>
+  import SideBar from "./components/sidebar/SideBar.vue";
+  import NavigationBar from "./components/navbar/NavigationBar.vue";
+  import AdminPage from "./views/SchoolAdmin.vue";
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
