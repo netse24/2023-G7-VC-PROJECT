51407e20f95152b65a7bf5b8ce4002983882ba2c
SOKI.CHHUOY <SOKI.CHHUOY@PNC.LAN>
2023-07-05 14:24:56 +0700

    [SMSG-41] update form login

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <router-view/>
+  <LoginView @add-form="login"></LoginView>
 </template>
 
+<script>
+import LoginView from './views/login/LoginView.vue'
+export default {
+  components:{
+    LoginView
+  },
+
+  methods:{
+    login(login){
+      alert(login)
+
+    }
+  }
+
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
