df0f4734061791bc495315c5b96dc220be3ab7d0
THAVROTH.CHEA <THAVROTH.CHEA@PNC.LAN>
2023-07-06 17:43:21 +0700

    [SMSG-42]:Form create student or teacher

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,7 +9,7 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 // import LoginView from './views/login/LoginView.vue'
-import UserForm from "./views/admin/user-form/UserForm.vue"
+import UserForm from "./views/admin/form/UserForm.vue"
 
 const app = createApp(App);
 const vuetify = createVuetify({
