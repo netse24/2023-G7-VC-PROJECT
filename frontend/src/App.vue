f068b794f9949e5b0d85695457bd5dbee8d21eaf
THAVROTH.CHEA <THAVROTH.CHEA@PNC.LAN>
2023-07-06 10:55:26 +0700

    [SMSG-42]: Delete file form create user

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <router-view/>
+  <!-- <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <router-view/>
+  </nav> -->
+  <user-form @add-emit="add"></user-form>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+
+export default{
+    data(){
+      return{
+        address: "",
+      }
+    },
+    methods: {
+      add(gender){
+        console.log(gender)
+      }
+    }
 }
 
-nav {
-  padding: 30px;
-}
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './style.css'
 
 // Vuetify
 import 'vuetify/styles'
@@ -8,12 +9,15 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 // import LoginView from './views/login/LoginView.vue'
+import UserForm from "./views/admin/user-form/UserForm.vue"
 
 const app = createApp(App);
 const vuetify = createVuetify({
   components,
   directives,
 })
+
+app.component('UserForm', UserForm);
 
 app.use(vuetify)
 app.use(router)
