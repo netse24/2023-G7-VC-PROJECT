d5137998d3845b18c9d3c3b9843b36052cb750ce
NetSe24 <netse0962@gmail.com>
2023-07-10 13:23:33 +0700

    solve confix

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,78 +1,12 @@
 
 <template>
-<<<<<<< HEAD
-<<<<<<< HEAD
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-    <router-view/>
-  </nav>
-=======
->>>>>>> a48eb9e84aa63d24e3257b38430929f08df1404d
   <v-app>
     <v-main>
       <router-view></router-view>
     </v-main>
   </v-app>
-=======
-  <!-- <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-    <router-view/>
-  </nav> -->
-  <form-create-user @user-emit="createUser" @teacher-emit="createTeacher"></form-create-user>
->>>>>>> createUser
 </template>
 <script>
-<<<<<<< HEAD
-<<<<<<< HEAD
-// import LoginView from './views/login/LoginView.vue'
-export default {
-=======
-import axios from "axios";
-import FormCreateUser from "./views/FormCreateUser.vue"
-// import LoginView from './views/login/LoginView.vue'
-export default {
-  components:{
-    FormCreateUser
-  },
->>>>>>> createUser
-
-  methods:{
-    login(login){
-      alert(login)
-    },
-    createUser(userInfo){
-      axios.post("http://127.0.0.1:8000/api/user", userInfo)
-              .then(response=>{
-                  console.log(response.data)
-              })
-              .catch(error=>{
-                  console.log(error)
-    })
-    },
-    createTeacher(teacherInfo){
-      console.log(teacherInfo)
-      axios.post("http://127.0.0.1:8000/api/teacher", teacherInfo)
-              .then(response=>{
-                  console.log(response.data)
-              })
-              .catch(error=>{
-              console.log(error)
-      })
-      },
-  }
-  
-
-}
-<<<<<<< HEAD
-</script>
-=======
-</script>
-
-
->>>>>>> createUser
-=======
 import { userInformations } from '@/store/userStore'
 export default {
   name: 'App',
@@ -87,4 +21,3 @@ export default {
 
 </script>
 
->>>>>>> a48eb9e84aa63d24e3257b38430929f08df1404d
