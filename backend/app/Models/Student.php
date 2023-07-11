91c91bcd49aa0b32f3a3621f01f3413e5d6f7509
dariyathorn <dariya.thorn@student.passerellesnumeriques.org>
2023-07-11 19:40:32 +0700

    [SMSG-62]:BackEnd test get student list

--- backend/app/Models/Student.php
+++ backend/app/Models/Student.php
@@ -5,10 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-<<<<<<< HEAD
-
-=======
->>>>>>> 8b0cbb3369cb496158f105529760d10b0b9fddf9
 class Student extends Model
 {
     use HasFactory;
@@ -32,8 +28,4 @@ class Student extends Model
         $student = self::updateOrCreate(['id' => $id], $student);
         return $student;
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 8b0cbb3369cb496158f105529760d10b0b9fddf9
 }
