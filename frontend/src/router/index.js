267532bbe2fa78577874232824e6b30cd072dc8d
THAVROTH.CHEA <THAVROTH.CHEA@PNC.LAN>
2023-07-20 13:29:37 +0700

    [SMSG-168]:Solve conflict

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -58,26 +58,27 @@ const routes = [
     path: '/admin/generations/studentList/:id',
     name: 'studentList',
     component: () => import('../views/admin/StudentListView.vue'),
-    meta: {
-      requireAuth: true,
-      token: token,
-      role: isAdmin
-    },
+    beforeEnter: [isUserLoginRequired, isUserRoleRequired('admin')],
+    props: true,
+  },
+  {
+    path: '/admin/students/detail/:student_id',
+    name: 'admin-students-detail',
+    component: () => import('../views/student/StudentDetailView.vue'),
+    beforeEnter: [isUserLoginRequired, isUserRoleRequired('admin')],
+    props: true
+  },
+  {
+    path: '/admin/teachers',
+    name: 'admin-teachers',
+    component: () => import('../views/admin/TeacherListView.vue'),
+    beforeEnter: [isUserLoginRequired, isUserRoleRequired('admin')],
   },
   {
     path: '/admin/teachers/detail/:teacher_id',
     name: 'admin-teachers-detail',
     component: () => import('../views/teacher/TeacherDetailView.vue'),
-  },
-  {
-    path: '/admin/students/detail',
-    name: 'admin-students-detail',
-    component: () => import('../views/student/StudentDetailView.vue'),
-    meta: {
-      requireAuth: true,
-      token: token,
-      role: isAdmin
-    },
+    beforeEnter: [isUserLoginRequired, isUserRoleRequired('admin')],
     props: true
   },
 
@@ -117,7 +118,14 @@ const routes = [
 
   },
   {
-    path: '/student/schedule',
+    path: '/students/background/:id',
+    name: 'student-background',
+    component: () => import('../views/student/StudentBackground.vue'),
+    props: true,
+    beforeEnter: [isUserLoginRequired, isUserRoleRequired('student')]
+  },
+  {
+    path: '/students/schedule',
     name: 'student-schedule',
     beforeEnter: [isUserLoginRequired, isUserRoleRequired('student')]
 
