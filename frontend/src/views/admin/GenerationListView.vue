dcbda8decd8e371dc25af6b5f09e0b64fda31867
pinatacoding <pinataseokmatthewcoding@gmail.com>
2023-07-27 14:58:30 +0700

    [SMSG-168]:update style

--- frontend/src/views/admin/GenerationListView.vue
+++ frontend/src/views/admin/GenerationListView.vue
@@ -3,18 +3,34 @@
     <nav-bar />
     <div class="d-flex flex-col p-10">
       <div class="flex gap-2">
-        <button class="bg-cyan-500 hover:bg-cyan-600 font-bold py-2 px-4 rounded mb-4">
+        <button
+          class="bg-cyan-500 hover:bg-cyan-600 font-bold py-2 px-4 rounded mb-4"
+        >
           <router-link to="/admin">Home</router-link>
         </button>
+        <div class="flex justify-center m-auto">
+          <h1 class="text-3xl font-bold  -ml-[7.5rem]">
+            All Generation of the Students
+          </h1>
+        </div>
       </div>
-      <div class="flex justify-between mb-3">
-        <h1 class="text-3xl m-auto font-bold mb-3">All Generation of the Students</h1>
-      </div>
-      <div class="w-75 m-auto border border-1 h-50 p-10 rounded shadow-[10px_35px_150px_-2px_rgba(0,0,0,0.3)]">
-        <div class="grid grid-cols-8 gap-2">
-          <button v-for="(generation, index) in generationList" :key="index" @click="showlistStudent(generation.id)"
-            class="bg-orange-500 pb-1 rounded-lg">
-            <v-text class="bg-cyan-500 hover:bg-cyan-600 py-3 px-4 text-2xl font-bold rounded-lg flex justify-center"> 
+
+      <div
+        class="w-75 m-auto border border-1 p-10 rounded shadow-[10px_35px_150px_-2px_rgba(0,0,0,0.3)]"
+      >
+        <div
+          class="grid grid-cols-8 gap-2"
+          style="height: 50vh; overflow: auto"
+        >
+          <!-- grid grid-cols-8 gap-2 -->
+          <button
+            v-for="(generation, index) in generationList"
+            :key="index"
+            @click="showlistStudent(generation.id)"
+          >
+            <v-text
+              class="bg-cyan-500 hover:bg-cyan-600 py-3 px-4 text-2xl font-bold rounded-lg flex justify-center border-b-4 border-b-orange-600"
+            >
               {{ generation.name }}
             </v-text>
           </button>
@@ -26,13 +42,13 @@
 <script>
 import { axiosClient } from "../../axios-http";
 import { storeManageCookie } from "@/store/cookie";
-import { AES, enc } from 'crypto-js';
+import { AES, enc } from "crypto-js";
 export default {
   setup() {
-    const userCookie = storeManageCookie()
+    const userCookie = storeManageCookie();
     return {
-      userCookie
-    }
+      userCookie,
+    };
   },
   data() {
     return {
@@ -51,14 +67,17 @@ export default {
         });
     },
     showlistStudent(id) {
-      const role = AES.decrypt(this.userCookie.getCookie("user_role"), "Secret role").toString(enc.Utf8);
+      const role = AES.decrypt(
+        this.userCookie.getCookie("user_role"),
+        "Secret role"
+      ).toString(enc.Utf8);
       // alert(role)
-      if (role == 'admin') {
-        this.$router.push(`/admin/generations/studentList/${id}`)
-      } else if (role == 'teacher') {
-        this.$router.push(`/teacher/generations/studentList/${id}`)
+      if (role == "admin") {
+        this.$router.push(`/admin/generations/studentList/${id}`);
+      } else if (role == "teacher") {
+        this.$router.push(`/teacher/generations/studentList/${id}`);
       }
-    }
+    },
   },
   mounted() {
     this.getGenerationList();
