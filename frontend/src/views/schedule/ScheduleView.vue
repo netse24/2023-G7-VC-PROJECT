342debe1084287b4cdcc00d13cbe0a282d92009b
soknoeurn-srun <soknoeurn.srun@student.passerellesnumeriques.org>
2023-07-27 13:13:24 +0700

    [SMSG-168] add navbar in schedule

--- frontend/src/views/schedule/ScheduleView.vue
+++ frontend/src/views/schedule/ScheduleView.vue
@@ -1,6 +1,7 @@
 // Reference from FullCalendar library => https://fullcalendar.io/ //
 <template>
   <section>
+    <navigationBar/>
     <form class="w-75 m-auto mt-3">
       <div class="mb-3">
         <select
