2bf6c7cf0ff2e1bd7284d2fa4070f47083953ddd
NetSe24 <netse0962@gmail.com>
2023-07-29 20:44:01 +0700

    fix style

--- frontend/src/views/teacher/TeacherDetailView.vue
+++ frontend/src/views/teacher/TeacherDetailView.vue
@@ -21,7 +21,7 @@
           {{ teachers.user.address }}
         </p>
       </div>
-      <div class="border-gray-200 rounded-lg shadow-xl w-5/12">
+      <div class="border-gray-200 rounded-lg shadow-xl w-1/2">
         <p class="flex justify-center text-3xl font-bold dark:text-dark p-6 rounded-t-lg bg-cyan-500">
           Teacher Information 
         </p>
